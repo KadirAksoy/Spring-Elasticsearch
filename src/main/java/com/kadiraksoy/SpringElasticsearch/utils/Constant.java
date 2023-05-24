95d454ae98d3b0ebc07a81ad2c60d1412be0ad12
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-24 11:14:30 +0300

    utils class added

---
+++ src/main/java/com/kadiraksoy/SpringElasticsearch/utils/Constant.java
@@ -0,0 +1,8 @@
+package com.kadiraksoy.SpringElasticsearch.utils;
+
+public class Constant {
+    public static final String MOVIE_NAME = "name";
+    public static final String MOVIE_ID = "name";
+    public static final String MOVIE_DIRECTOR = "director.name";
+}
+
